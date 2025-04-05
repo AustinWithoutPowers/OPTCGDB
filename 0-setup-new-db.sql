ffdc4e23449b4caa5e9a54163ee9df72341878cd
Austin Rudkin <austinrudkin@live.com>
2025-04-05 21:05:32 +1300

    Initial commit

---
+++ 0-setup-new-db.sql
@@ -0,0 +1,76 @@
+-- CREATE DATABASE [OPTCGDB] COLLATE Latin1_General_CI_AI;
+-- GO
+
+USE [OPTCGDB];
+GO
+
+DROP TABLE IF EXISTS [Cards];
+DROP TABLE IF EXISTS [Sets];
+DROP TABLE IF EXISTS [SetTypes];
+DROP TABLE IF EXISTS [CardTypes];
+DROP TABLE IF EXISTS [Colours];
+DROP TABLE IF EXISTS [Rarities];
+DROP TABLE IF EXISTS [Attributes];
+
+CREATE TABLE [Attributes] (
+	[AttributeID] NCHAR NOT NULL UNIQUE,
+	[AttributeName] NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE [Rarities] (
+	RarityID NVARCHAR(4) NOT NULL UNIQUE,
+	RarityName NVARCHAR(32) NOT NULL
+)
+
+CREATE TABLE [Colours] (
+	[ColourID] NCHAR NOT NULL UNIQUE,
+	[ColourName] NVARCHAR(16) NOT NULL,
+	CONSTRAINT [PK_Colours] PRIMARY KEY ([ColourID])
+)
+
+CREATE TABLE [CardTypes] (
+	[CardTypeID] INT IDENTITY(0, 1) NOT NULL UNIQUE,
+	[CardTypeName] NVARCHAR(32) NOT NULL,
+	CONSTRAINT [PK_CardTypes] PRIMARY KEY ([CardTypeID])
+)
+
+CREATE TABLE [SetTypes] (
+	[SetTypeID] INT IDENTITY(0, 1) NOT NULL UNIQUE,
+	[SetTypeName] NVARCHAR(64) NOT NULL,
+	[SetTypeShortCode] NVARCHAR(16) NOT NULL,
+	CONSTRAINT [PK_SetTypes] PRIMARY KEY ([SetTypeID])
+)
+
+CREATE TABLE [Sets] (
+	[SetID] INT IDENTITY(0, 1) NOT NULL UNIQUE,
+	[SetName] NVARCHAR(64) NOT NULL,
+	[SetCode] NVARCHAR(16) NOT NULL,
+	[SetTypeID] INT,
+	[ReleaseDate] DATE,
+	CONSTRAINT [PK_Sets] PRIMARY KEY ([SetID]),
+	CONSTRAINT [FK_Sets_SetTypes] FOREIGN KEY ([SetTypeID]) REFERENCES [SetTypes] ([SetTypeID])
+)
+
+CREATE TABLE [Cards] (
+	[CardID] NVARCHAR(16) NOT NULL UNIQUE,
+	[CardName] NVARCHAR(64) NOT NULL,
+	[SetID] INT NOT NULL,
+	[CardTypeID] INT NOT NULL,
+	[ColourID] NCHAR NOT NULL,
+	[SecondColourID] NCHAR NULL,
+	[Cost] SMALLINT NULL,
+	[Power] INT NULL,
+	[Life] SMALLINT NULL,
+	[RarityID] NVARCHAR(4) NOT NULL,
+	[AltArt] BIT NOT NULL, -- Unsure if we need to change this to be nullable...?
+	[AttributeID] NCHAR NOT NULL,
+	CONSTRAINT [PK_Cards] PRIMARY KEY ([CardID]),
+	CONSTRAINT [FK_Cards_Sets] FOREIGN KEY ([SetID]) REFERENCES [Sets] ([SetID]),
+	CONSTRAINT [FK_Cards_CardTypes] FOREIGN KEY ([CardTypeID]) REFERENCES [CardTypes] ([CardTypeID]),
+	CONSTRAINT [FK_Cards_Colours] FOREIGN KEY ([ColourID]) REFERENCES [Colours] ([ColourID]),
+	CONSTRAINT [FK_Cards_Colours_Second] FOREIGN KEY ([ColourID]) REFERENCES [Colours] ([ColourID]),
+	CONSTRAINT [FK_Cards_Rarities] FOREIGN KEY ([RarityID]) REFERENCES [Rarities] ([RarityID]),
+	CONSTRAINT [FK_Cards_Attributes] FOREIGN KEY ([AttributeID]) REFERENCES [Attributes] ([AttributeID])
+)
+
+SELECT GETDATE() AS [GETDATE( )]
